fbd04eb4f686e671a918ac5c7686b4b72e981918
Thomas <amanda.batista.silva23@escola.pr.gov.br>
2026-05-04 11:04:34 -0300

    carstore

---
+++ script/scritp.js
@@ -0,0 +1,10 @@
+function  salvarCarro(event) {
+    event.Default();
+
+    let titulo = document.getElementById(`title`).value;
+    let titulo = document.getElementById(`preco`).value;
+    let titulo = document.getElementById(`marca`).value;
+    let titulo = document.getElementById(`modelo`).value;
+
+    
+}
